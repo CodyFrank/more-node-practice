25979e222d2b97303c499658f32d702443ede77b
Cody Frank <cody.frank30@gmail.com>
2020-06-15 13:47:01 -0400

    adds server error handling

--- index.js
+++ index.js
@@ -41,7 +41,23 @@ const server = http.createServer((req, res) => {
             break
     }
 
-    fs.readFile()
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT'){
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, { 'contentType': 'text.html' })
+                    res.end(content, 'udf8')
+                })
+            } else {
+                res.writeHead(500),
+                res.end(`Server Error: ${err.code}`)
+            } 
+        } else {
+            res.writeHead(200, { 'content-type': 'text.html' })
+            res.end(content, 'utf8')
+        }
+
+    })
 
 
 })
