bcf08318feb86d6a45048266a5a00b00f09f363d
Cody Frank <cody.frank30@gmail.com>
2020-06-15 09:14:34 -0400

    reads file

--- referances/fs-demo.js
+++ referances/fs-demo.js
@@ -3,17 +3,24 @@ const path = require('path')
 
 
 // create folder
-fs.mkdir(path.join(__dirname, 'test'), {}, err => {
-    if(err) throw err
-    console.log('folder created')
-} )
+// fs.mkdir(path.join(__dirname, 'test'), {}, err => {
+//     if (err) throw err
+//     console.log('folder created')
+// } )
 
 // create and write to file 
-fs.writeFile(path.join(__dirname, 'test', 'hello.txt'), "Hello World", err => {
+// fs.writeFile(path.join(__dirname, 'test', 'hello.txt'), "Hello World", err => {
+//     if (err) throw err
+//     console.log('file written too')
+//     fs.appendFile(path.join(__dirname, 'test', 'hello.txt'), " from node.js", err => {
+//         if (err) throw err
+//         console.log('file appended')
+//     } ) 
+// })
+
+// reads file (note this is asynchrones meaning if you run this whole file it could give you unexpected results because of this
+// readfile should be used on a prexisting file not one that may still be written too or may still be created 
+fs.readFile(path.join(__dirname, 'test', 'hello.txt'), 'utf8', (err, data) => {
     if (err) throw err
-    console.log('file written too')
-    fs.appendFile(path.join(__dirname, 'test', 'hello.txt'), " from node.js", err => {
-        if (err) throw err
-        console.log('file appended')
-    } ) 
+    console.log(data)
 })
