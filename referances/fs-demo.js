6708219906b03312974cd184f4f20150152faff2
Cody Frank <cody.frank30@gmail.com>
2020-06-15 09:27:55 -0400

    renamed file

--- referances/fs-demo.js
+++ referances/fs-demo.js
@@ -20,7 +20,14 @@ const path = require('path')
 
 // reads file (note this is asynchrones meaning if you run this whole file it could give you unexpected results because of this
 // readfile should be used on a prexisting file not one that may still be written too or may still be created 
-fs.readFile(path.join(__dirname, 'test', 'hello.txt'), 'utf8', (err, data) => {
-    if (err) throw err
-    console.log(data)
-})
+// fs.readFile(path.join(__dirname, 'test', 'hello.txt'), 'utf8', (err, data) => {
+//     if (err) throw err
+//     console.log(data)
+// })
+
+
+fs.rename(path.join(__dirname, 'test', 'hello.txt'), path.join(__dirname, 'test', 'hello-world.txt'), err => {
+    if(err) throw err
+    console.log('file renamed')
+    }
+)
